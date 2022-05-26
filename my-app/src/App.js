98a22b33597bad488e8819f616fa9cb67b8e5f1e
RubenBaader <rubenbaader@gmail.com>
2022-05-26 16:05:08 +0200

    Learning create-react-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
+// first step: fetch data for quotes
+// second step: store data in a constant
+// third step: add state and extract relevant data point to that state
+// fourth step: display the quotes from state
 
-fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
-  .then (response => response.json())
-  .then (data => console.log(data));
+function QuoteGetter() {
+  fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    .then (response => response.json())
+    // .then(data => data)
+}
+console.log(QuoteGetter)
 
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      "quote": "hiii",
+      "author": ""
+    }
+  }
+  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello
-        </a>
-      </header>
-    </div>
-  );
+  // let myQuotes = {"hello":"world"};
+  // console.log("look here", myQuotes);
+  render() {
+    return (
+      
+      <div className="App">
+        <header className="App-header">
+          <p>
+            This is an app. {this.state.quote}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
