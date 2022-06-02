eefb82b7bc702687e1cee1afd64a83f77f7167ee
RubenBaader <rubenbaader@gmail.com>
2022-06-02 17:10:38 +0200

    User stories complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,65 @@
 import './App.css';
 import React, { Component } from 'react';
 
-// first step: fetch data for quotes
-// second step: store data in a constant
-// third step: add state and extract relevant data point to that state
-// fourth step: display the quotes from state
+/*  */
 
-function QuoteGetter() {
-  fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+const URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+
+function QuoteGetter(url) {
+  return fetch(url)
     .then (response => response.json())
-    // .then(data => data)
 }
-console.log(QuoteGetter)
+
+async function QuoteSetter(Callback, url) {
+  let quoteData = {};
+  quoteData = await Callback(url)
+  let i = Math.floor(Math.random() * quoteData["quotes"].length)
+  return quoteData["quotes"][i];
+}
 
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
       "quote": "hiii",
-      "author": ""
-    }
+      "author": "unknown"
+    };
+    this.handleClick = this.handleClick.bind(this); 
   }
   
+  componentDidMount(){
+    QuoteSetter(QuoteGetter, URL).then(data => this.setState(data));
 
-  // let myQuotes = {"hello":"world"};
-  // console.log("look here", myQuotes);
+/*     // Quick and dirty implementation of unit tests
+      const s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js';
+      document.body.appendChild(s); 
+    // End unit test */
+  };
+
+  handleClick(){
+    QuoteSetter(QuoteGetter, URL).then(data => this.setState(data))
+  };
+  
   render() {
     return (
-      
       <div className="App">
-        <header className="App-header">
-          <p>
-            This is an app. {this.state.quote}
+        <div className="App-header" id='quote-box'>
+          <p id='text'>
+            {this.state.quote}
           </p>
-        </header>
+          <p id='author'>
+            -- {this.state.author}
+          </p>
+          <div className='button-box'>
+            <button onClick={this.handleClick} id='new-quote' >New Quote</button>
+            <a 
+            href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=` + this.state.quote + ` -- ` + this.state.author} 
+            target="_blank" rel='noreferrer' id='tweet-quote' >Tweet Quote</a>
+          </div>
+        </div>
       </div>
     );
   }
